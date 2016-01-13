68f83fdc37578277ec0c151b123dc9197697f02b
CPChang <chang.pao.chang@gmail.com>
2016-01-13 14:02:09 -0600

    1st commit, NOT TESTED

---
+++ cmake_build.bat
@@ -0,0 +1,11 @@
+:: This batch use cmake to generate the dealii project.
+:: the default path of build is inside the pwd. 
+:: This can be used as right click menu, say "cmake build here"
+
+"C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2016\windows\mkl\bin\mklvars.bat" ia32
+SET BOOST_DIR=C:/local/boost_1_56_0
+SET BOOST_LIBRARYDIR=C:/local/boost_1_56_0/lib64-msvc-12.0
+mkdir build
+cd build
+"C:\Program Files (x86)\CMake\bin\cmake.exe" -G "Visual Studio 12 2013" .. -DDEAL_II_DIR="F:/CBC_build/dealii/install"
+
---
+++ load_env.bat
@@ -0,0 +1,9 @@
+:: 1st arg: ${target}, which will be the target exe when debugging.
+:: 
+:: The default is for msvc
+
+SET BOOST_DIR=C:/local/boost_1_56_0
+SET BOOST_LIBRARYDIR=C:/local/boost_1_56_0/lib64-msvc-12.0
+SET cmake="C:\Program Files (x86)\CMake\bin\cmake.exe"
+"C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2016.0.110\windows\bin\ipsxe-comp-vars.bat" ia32 vs2013
+IF not "%1"=="" (%1)
