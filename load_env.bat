a0468a0959a21217bf1f0d88c7538fec585f460d
CPChang <chang.pao.chang@gmail.com>
2016-01-15 08:17:30 -0600

    add boost support

--- load_env.bat
+++ load_env.bat
@@ -2,8 +2,12 @@
 :: 
 :: The default is for msvc
 
+SETLOCAL
+
 SET BOOST_DIR=C:/local/boost_1_56_0
 SET BOOST_LIBRARYDIR=C:/local/boost_1_56_0/lib64-msvc-12.0
 SET cmake="C:\Program Files (x86)\CMake\bin\cmake.exe"
-"C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2016.0.110\windows\bin\ipsxe-comp-vars.bat" ia32 vs2013
-IF not "%1"=="" (%1)
+CALL "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2016.0.110\windows\bin\ipsxe-comp-vars.bat" ia32 vs2013
+CALL %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+ENDLOCAL
