724a6592081cda6695ee48432d51db61be3eedab
CPChang <chang.pao.chang@gmail.com>
2016-01-18 10:57:43 -0600

    finalize the bat program.

--- cmake_build.bat
+++ cmake_build.bat
@@ -2,14 +2,15 @@
 :: the default path of build is inside the pwd. 
 :: This can be used as right click menu, say "cmake build here"
 
-SETLOCAL
+:: SETLOCAL
 @echo off
+if "%1"=="" (set buildDir=build) else (set buildDir=%1)
 
+echo Build Dir: %buildDir%
 CALL "C:\Program Files (x86)\IntelSWTools\compilers_and_libraries_2016\windows\mkl\bin\mklvars.bat" ia32
-SET BOOST_ROOT=C:/local/boost_1_56_0
-SET BOOST_LIBRARYDIR=C:/local/boost_1_56_0/lib64-msvc-12.0
-mkdir build
-cd build
-CALL "C:\Program Files (x86)\CMake\bin\cmake.exe" -G "Visual Studio 12 2013" .. -DDEAL_II_DIR="F:/CBC_build/dealii/install"
+mkdir %buildDir%
+cd %buildDir%
+CALL "C:\Program Files (x86)\CMake\bin\cmake.exe" -G "Visual Studio 12 2013" .. -DDEAL_II_DIR="F:/CBC_build/dealii/install-x86-vs2013-tjhei-bundledboost-notbb-shareoff" -DBOOST_ROOT=C:/local/boost_1_56_0 -DBOOST_LIBRARYDIR=C:/local/boost_1_56_0/lib32-msvc-12.0 
 
-ENDLOCAL
+cd ..
+:: ENDLOCAL
